975f93dbfeb00fee134e238d22d47ebe44ebb9e0
Andrei E <andreien@protonmail.com>
2020-08-23 15:42:00 +0100

    Implement random and fixed colo(u)rs.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,18 @@ function clearEtch() {
     let etch = document.querySelector('#etch');
     etch.innerHTML = '';
 }
+function decideColor(randomColors) {
+    if (randomColors === 'r') {
+        let red = Math.floor(255 * Math.random());
+        let green = Math.floor(255 * Math.random());
+        let blue = Math.floor(255 * Math.random());
+        let alpha = Math.floor(Math.random() + 0.5); // Minimum of 0.5, to keep high values.
+        return `rgba(${red}, ${green}, ${blue}, ${alpha})`;   
+    }
+    else {
+        return document.querySelector('#color').value;
+    }
+}
 function createColumns(columnCount) {
     let etch = document.querySelector('#etch');
     for (i = 0; i < columnCount; i++) {
@@ -23,25 +35,26 @@ function createRows(rowCount, columnCount) {
         }
     })
 }
-function createDivs(rowCount, columnCount) {
+function createDivs(rowCount, columnCount, randomColors) {
     clearEtch();
     createColumns(columnCount);
     createRows(rowCount, columnCount);
-    changeColor('black');
+    changeColor(randomColors);
 }
-function changeColor(color) {
+function changeColor(randomColors) {
     rows = document.querySelectorAll('.row')
     rows.forEach(function (row) {
-        row.addEventListener('mouseover', () => {row.style.backgroundColor = color})
+        row.addEventListener('mouseover', () => { row.style.backgroundColor = decideColor(randomColors) })
     })
 }
 function changeProperties() {
     let rowCount = prompt("How many rows?", 16);
     let columnCount = prompt("How many columns?", 16);
-    // Potentially to be implemented.
-    let randomColors = prompt("(R)andom colors or (f)ixed colors?", "R");
-    createDivs(rowCount, columnCount)
+    // Take strictly the first letter, in lowercase.
+    let randomColors = prompt("(R)andom colors or (f)ixed colors?", "r").toLowerCase()[0];
+    createDivs(rowCount, columnCount, randomColors)
 }
-createDivs(16, 16);
+let colorPicked = '#000000'
+createDivs(16, 16, 'r');
 let propertiesButton = document.querySelector('#properties');
 propertiesButton.addEventListener("click", changeProperties);
